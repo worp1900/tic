28617a446492634db0ed5abd6fd83503d0ec6d29
Helge Reelfs <info@iotduino.de>
2016-05-24 19:20:35 +0200

    removes dead code

--- tic/sessionhelpers.inc.php
+++ tic/sessionhelpers.inc.php
@@ -30,11 +30,11 @@ function check_user($name, $pass) {
 	$iplock = mysql_fetch_assoc($SQL_Result_iplock);
 	if (!$iplock)
 		return false;
-	
+
 	if($iplock['ip'] == $_SERVER['REMOTE_ADDR'] && $iplock['versuche'] >= 3)
 		die ('Dieser Account ist gesperrt, wenden sie sich an Ihren Adminstrator');
 	mysql_free_result($SQL_Result_iplock);
-	
+
 	$SQL_Query = "SELECT id FROM gn4accounts WHERE name='".$name."' AND passwort=MD5('".$pass."') LIMIT 1;";
 	$SQL_Result_login = mysql_query($SQL_Query) or die(mysql_errno()." - ".mysql_error());
 	if ($user = mysql_fetch_assoc($SQL_Result_login)) {
@@ -43,38 +43,11 @@ function check_user($name, $pass) {
 		mysql_query($SQL_Query) or die(mysql_errno()." - ".mysql_error());
 		return $user['id'];
 	}
-	
+
 	$SQL_Query = "UPDATE gn4accounts SET versuche=versuche + 1, ip='".$_SERVER['REMOTE_ADDR']."' WHERE name='".$name."';";
 	mysql_query($SQL_Query) or die(mysql_errno()." - ".mysql_error());
 	return false;
 }
-
-/* werden derzeit nicht gebraucht
-function login($userid)
-{
-    $sql="UPDATE gn4accounts
-    SET session='".session_id()."'
-    WHERE id=".$userid." and ticid=".$ticid;
-     mysql_query($sql);
-}
-
-function logged_in()
-{
-    $sql="SELECT id
-    FROM gn4accounts
-    WHERE session='".session_id()."'
-    LIMIT 1";
-    $result= mysql_query($sql);
-      return ( mysql_num_rows($result)==1);
-}
-
-function logout()
-{
-    $sql="UPDATE gn4accounts
-    SET session=NULL
-    WHERE session='".session_id()."'";
-     mysql_query($sql);
-}*/
 
 $SQL_DBConn = connect();
 ?>
