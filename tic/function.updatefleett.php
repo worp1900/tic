f243f95bde67f4e7325e39d97f4e8b2736df1e9b
Helge Reelfs <info@iotduino.de>
2016-05-29 17:43:23 +0200

    make the timestamp update not using any variable

--- tic/function.updatefleett.php
+++ tic/function.updatefleett.php
@@ -1,8 +1,7 @@
 <?php
 	//update timestamp for gal-fleetimport
-	$sql = "INSERT INTO gn4galfleetupdated (gal, t, erfasser) VALUES (" . $Benutzer['galaxie'] . ", unix_timestamp(now()), '" . $Benutzer['name'] . "')
-ON DUPLICATE KEY UPDATE t=unix_timestamp(now()), erfasser='" . $Benutzer['name'] . "';";
-	tic_mysql_query($sql, $SQL_DBConn);
+	tic_mysql_query("INSERT INTO gn4galfleetupdated (gal, t, erfasser) VALUES (" . $Benutzer['galaxie'] . ", unix_timestamp(now()), '" . $Benutzer['name'] . "')
+ON DUPLICATE KEY UPDATE t=unix_timestamp(now()), erfasser='" . $Benutzer['name'] . "';", __FILE__, __LINE__);
 
 if(!isset($doNotRelocate)) {
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
